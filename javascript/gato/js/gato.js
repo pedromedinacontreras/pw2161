4df9bbf06e6a57463e344d645189403ef07c923a
Pedro <pmedina.c95@gmail.com>
2016-04-20 10:59:33 -0600

    Gato funcionando

--- javascript/gato/js/gato.js
+++ javascript/gato/js/gato.js
@@ -1,4 +1,4 @@
-var contador=0;
+var contador=1;
 
 function seleccion(valor,pos)
 {
@@ -14,5 +14,68 @@ function seleccion(valor,pos)
 			document.getElementById(pos).value="O";
 			contador=contador+1;
 		}
+
+		if(contador>=5)
+		checar();
 	}
+}
+
+function checar()
+{
+	if(contador>=5)
+	{
+	if((document.getElementById("s11").value==document.getElementById("s12").value)&&(document.getElementById("s11").value==document.getElementById("s13").value)&&(document.getElementById("s11").value!=""))
+	{
+		alert("El jugador "+document.getElementById("s11").value+" ha ganado.");
+		location.reload();
+	}
+
+	if((document.getElementById("s21").value==document.getElementById("s22").value)&&(document.getElementById("s21").value==document.getElementById("s23").value)&&(document.getElementById("s21").value!=""))
+	{
+		alert("El jugador "+document.getElementById("s21").value+" ha ganado.");
+		location.reload();
+	}
+
+	if((document.getElementById("s31").value==document.getElementById("s32").value)&&(document.getElementById("s31").value==document.getElementById("s33").value)&&(document.getElementById("s31").value!=""))
+	{
+		alert("El jugador "+document.getElementById("s31").value+" ha ganado.");
+		location.reload();
+	}
+
+	if((document.getElementById("s11").value==document.getElementById("s21").value)&&(document.getElementById("s11").value==document.getElementById("s31").value)&&(document.getElementById("s11").value!=""))
+	{
+		alert("El jugador "+document.getElementById("s11").value+" ha ganado.");
+		location.reload();
+	}
+
+	if((document.getElementById("s12").value==document.getElementById("s22").value)&&(document.getElementById("s12").value==document.getElementById("s32").value)&&(document.getElementById("s12").value!=""))
+	{
+		alert("El jugador "+document.getElementById("s12").value+" ha ganado.");
+		location.reload();
+	}
+
+	if((document.getElementById("s13").value==document.getElementById("s23").value)&&(document.getElementById("s13").value==document.getElementById("s33").value)&&(document.getElementById("s13").value!=""))
+	{
+		alert("El jugador "+document.getElementById("s13").value+" ha ganado.");
+		location.reload();
+	}
+
+	if((document.getElementById("s11").value==document.getElementById("s22").value)&&(document.getElementById("s11").value==document.getElementById("s33").value)&&(document.getElementById("s11").value!=""))
+	{
+		alert("El jugador "+document.getElementById("s11").value+" ha ganado.");
+		location.reload();
+	}
+
+	if((document.getElementById("s13").value==document.getElementById("s22").value)&&(document.getElementById("s13").value==document.getElementById("s31").value)&&(document.getElementById("s13").value!=""))
+	{
+		alert("El jugador "+document.getElementById("s13").value+" ha ganado.");
+		location.reload();
+	}
+	}
+	if(contador==10)
+	{
+		alert("EMPATEEEEEE!!");
+		location.reload();
+	}
+	
 }
